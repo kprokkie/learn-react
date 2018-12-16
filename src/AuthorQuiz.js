3b80f614524a8c645f081d6b84d9a3d72cd204a1
Pradeep Kumar <kumarpradeep1010@gmail.com>
2018-12-15 20:10:26 -0600

    feat: setting up app files

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class AuthorQuiz extends Component {
+  render() {
+    return (
+      <div>Author Quiz</div>
+    );
+  }
+}
+
+export default AuthorQuiz;
