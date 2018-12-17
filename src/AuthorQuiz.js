58fa9843bcf088c5a0ee7415afcbd2c5db09e286
Pradeep Kumar <kumarpradeep1010@gmail.com>
2018-12-16 19:37:29 -0600

    feat: up and running quiz functionalty

--- src/AuthorQuiz.js
+++ src/AuthorQuiz.js
@@ -1,13 +1,101 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
+//import logo from './logo.svg';
+import './bootstrap.min.css';
 import './App.css';
 
-class AuthorQuiz extends Component {
-  render() {
-    return (
-      <div>Author Quiz</div>
-    );
-  }
+function Hero() {
+  return (
+    <div className="row">
+      <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>Select the book witten by author shown</p>
+      </div>
+    </div>
+  );
 }
+
+function Book({ title, onClick }) {
+  return (
+    <div className="answer" onClick={() => { onClick(title); }}>
+      <h4>{title}</h4>
+    </div>
+  );
+}
+
+function Turn({ author, books, highlight, onAnswerSelected }) {
+  function highlightBgColor(highlight) {
+    const mapping = {
+      'none': '',
+      'correct': 'green',
+      'wrong': 'red'
+    };
+    return mapping[highlight];
+  }
+
+  return (
+    <div className="row turn" style={{ backgroundColor: highlightBgColor(highlight) }}>
+      <div className="col-4 offset-1">
+        <img src={author.imageUrl} className="author-image" alt="Author" />
+        <span>{author.name}</span>
+      </div>
+      <div className="col-6">
+        {/* {books.map((title) => <p>{title}</p>)} */}
+        {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
+      </div>
+    </div>
+  );
+}
+
+Turn.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }),
+  books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onAnswerSelected: PropTypes.func.isRequired,
+  highlight: PropTypes.string.isRequired
+};
+
+function Continue() {
+  return (<div />);
+}
+
+function Footer() {
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">All images are from WWW.</p>
+      </div>
+    </div>
+  );
+}
+
+function AuthorQuiz({ turnData, highlight, onAnswerSelected }) {
+  console.log(turnData);
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
+      <Continue />
+      <Footer />
+    </div>
+  );
+}
+
+// class AuthorQuiz extends Component {
+//   render() { 
+//     return (
+//       <div className="container-fluid">
+//         <Hero />
+//         <Turn />
+//         <Continue />
+//         <Footer/>
+//       </div>
+//     );
+//   }
+// }
 
 export default AuthorQuiz;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,78 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
+import { shuffle, sample } from 'underscore';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
+// aplication data
 
+const authors = [
+    {
+        name: 'Rokkie 1',
+        imageUrl: '/images/authors/a1.JPG',
+        imageSrc: 'Rokkie Code',
+        books: ['Rokkie 11 ASDE', 'Rokkie 12 KDSL', 'Rokkie 13 JDLS', 'Rokkie 14 JDLL']
+    },
+    {
+        name: 'Rokkie 2',
+        imageUrl: '/images/authors/a2.JPG',
+        imageSrc: 'Rokkie Code',
+        books: ['Rokkie 21 ASDE', 'Rokkie 22 KDSL', 'Rokkie 23 JDLS', 'Rokkie 24 JDLL']
+    },
+    {
+        name: 'Rokkie 3',
+        imageUrl: '/images/authors/a3.JPG',
+        imageSrc: 'Rokkie Code',
+        books: ['Rokkie 31 ASDE', 'Rokkie 32 KDSL', 'Rokkie 33 JDLS', 'Rokkie 34 JDLL']
+    },
+    {
+        name: 'Rokkie 4',
+        imageUrl: '/images/authors/a4.JPG',
+        imageSrc: 'Rokkie Code',
+        books: ['Rokkie 41 ASDE', 'Rokkie 42 KDSL', 'Rokkie 43 JDLS', 'Rokkie 44 JDLL']
+    }
+];
+
+// const state = {
+//     turnData: {
+//         author: authors[0],
+//         books: authors[0].books
+//     }
+// };
+
+function getTurnData(authors) {
+    const allBooks = authors.reduce(function (p, c, i) {
+        return p.concat(c.books);
+    }, []);
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) =>
+            author.books.some((title) =>
+                title === answer))
+    }
+}
+
+const state = {
+    turnData: getTurnData(authors),
+    highlight: ''
+};
+
+function onAnswerSelected(answer) {
+    const isCorrect = state.turnData.author.books.some((book) => book === answer);
+    state.highlight = isCorrect ? 'correct' : 'wrong';
+    render();
+}
+
+// application rendered
+// ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'));
+
+function render() {
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={onAnswerSelected} />, document.getElementById('root'));
+}
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
